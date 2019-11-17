8f889a7e2386b22ca061bb49c1ccbb54e56d2c9d
Jang Belche <jangbel99@gmail.com>
2019-11-17 03:56:33 +0000

    see if it worked

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function cameraStart() {
 }
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
-    cameraSensor.style.visibility="hidden"
+    cameraView.srcObject = null;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
