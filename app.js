5e31de18a9f7cd3e3c3295787fd2c90e3b5e2a92
Jang Belche <jangbel99@gmail.com>
2019-11-17 11:10:20 +0000

    check

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+/**
 var x = document.getElementById("demo");
 
 
@@ -25,15 +25,15 @@ function cameraStart() {
 
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
-   
+
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    
+
     localStorage.setItem("pic", cameraView);
     cameraView.srcObject = null;
     var picture = localStorage.getItem("pic");
 
-    
+
     cameraOutput.classList.add("taken");
     window.location.replace("validation.html");
     //PLAY AROUND WITH THIS BUTTON!!
@@ -55,4 +55,124 @@ window.addEventListener("load", cameraStart, false);
 function loadPic(){
     console.log("picture loaded");
 
-}
+}*/
+var test = null;
+(function() {
+    var width = 320; // We will scale the photo width to this
+    var height = 0; // This will be computed based on the input stream
+
+    var streaming = false;
+
+    var video = null;
+    var canvas = null;
+    var photo = null;
+    var startbutton = null;
+
+    function startup() {
+        video = document.getElementById('video');
+        canvas = document.getElementById('canvas');
+        photo = document.getElementById('photo');
+        startbutton = document.getElementById('startbutton');
+        navigator.mediaDevices.getUserMedia({
+                video: true,
+                audio: false
+            })
+            .then(function(stream) {
+                video.srcObject = stream;
+                video.play();
+            })
+            .catch(function(err) {
+                console.log("An error occurred: " + err);
+            });
+        video.addEventListener('canplay', function(ev) {
+            if (!streaming) {
+                height = video.videoHeight / (video.videoWidth / width);
+
+                video.setAttribute('width', width);
+                video.setAttribute('height', height);
+                canvas.setAttribute('width', width);
+                canvas.setAttribute('height', height);
+                streaming = true;
+            }
+        }, false);
+        startbutton.addEventListener('click', function(ev) {
+            takepicture();
+            ev.preventDefault();
+        }, false);
+        clearphoto();
+    }
+
+    function clearphoto() {
+        var context = canvas.getContext('2d');
+        context.fillStyle = "#AAA";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        var data = canvas.toDataURL('image/jpeg');
+        photo.setAttribute('src', data);
+    }
+
+    function takepicture() {
+        console.log("Print");
+        var context = canvas.getContext('2d');
+        if (width && height) {
+            canvas.width = width;
+            canvas.height = height;
+            context.drawImage(video, 0, 0, width, height);
+
+            var data = canvas.toDataURL('image/jpeg');
+            test = data;
+
+            photo.setAttribute('src', data);
+        } else {
+            clearphoto();
+        }
+    }
+
+    window.addEventListener('load', startup, false);
+})();
+
+/*
+$(function() {
+    $("input:file").change(function (){
+        //var fileName = $(this).val();
+        //$(".filename").html(fileName);
+        $.ajax({
+            url: 'http://max-scene-classifier.max.us-south.containers.appdomain.cloud/model/predict',
+            method: 'POST',
+            data: {
+                "accept": "application/json",
+                "Content-Type": "multipart/form-data",
+                "image": document.getElementById("fileSelector").files[0]
+            },
+            success: function (response) {
+                    alert(response.status);
+            },
+            error: function (err) {
+                console.log(err);
+                alert("error");
+            }
+        })
+    });
+});*/
+
+$(function () {
+    $("#fileSelector").change(function () {
+        console.log("here");
+        $.ajax({
+            url: 'http://max-scene-classifier.max.us-south.containers.appdomain.cloud/model/predict',
+            method: 'POST',
+            data: {
+                "accept": "application/json",
+                "Content-Type": "multipart/form-data",
+                "image": document.getElementById("fileSelector").files[0]
+            },
+            success: function (response) {
+                    alert(response.status);
+            },
+            error: function (err) {
+                console.log(err);
+                alert("error");
+            }
+        })
+    })
+});
